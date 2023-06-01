5b055962ccfd9e1048cb9e159e2f2401633ec8b1
Umar Aqeel <112412509+Umar2838@users.noreply.github.com>
2023-06-02 02:17:03 +0500

    ghgm

--- app.js
+++ app.js
@@ -1,1 +1,78 @@
 
+var input = document.getElementById("postCard").style.display = "none";
+
+
+
+ function postcardopen(){
+
+document.getElementById("postCard").style.display = "block"
+     
+ }
+//  function postcardremove(){
+//     document.getElementById("postCard").style.display = "none"
+//  }
+
+function imagebackgroundpost1(){
+
+   var textarea = document.getElementById("textarea")
+   textarea.style.backgroundImage = "url('images/color change 1.jpg')"
+   textarea.style.color = "#fff"
+   textarea.style.textAlign = "center"
+   
+   
+
+ }
+ function imagebackgroundpost2(){
+
+   var textarea = document.getElementById("textarea")
+   textarea.style.backgroundImage = "url('images/color change 2.jpg')"
+   textarea.style.color = "#fff"
+   textarea.style.textAlign = "center"
+   
+
+ }
+ function imagebackgroundpost3(){
+
+   var textarea = document.getElementById("textarea")
+   textarea.style.backgroundImage = "url('images/color change 3.jpg')"
+   textarea.style.color = "#fff"
+   textarea.style.textAlign = "center"
+   
+
+ }
+ function imagebackgroundpost4(){
+
+   var textarea = document.getElementById("textarea")
+   textarea.style.backgroundImage = "url('images/color change 4.jpg')"
+   textarea.style.color = "#fff"
+   textarea.style.textAlign = "center"
+   
+
+ }
+ function imagebackgroundpost5(){
+
+   var textarea = document.getElementById("textarea")
+   textarea.style.backgroundImage = "url('images/color change 5.jpg')"
+   textarea.style.color = "#fff"
+   textarea.style.textAlign = "center"
+   
+
+ }
+ function imagebackgroundpost6(){
+
+   var textarea = document.getElementById("textarea")
+   textarea.style.backgroundImage = "url('images/color change 6.jpg')"
+   textarea.style.color = "#fff"
+   textarea.style.textAlign = "center"
+   
+
+ }
+ function imagebackgroundpost7(){
+
+   var textarea = document.getElementById("textarea")
+   textarea.style.backgroundImage = "url('images/color change 7.jpg')"
+   textarea.style.color = "#fff"
+   textarea.style.textAlign = "center"
+   
+
+ }
